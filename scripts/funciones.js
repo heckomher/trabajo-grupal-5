8e50468233a436bcc22d0c404958a610501f5820
Hector Komori <hector.komori@gmail.com>
2023-04-27 21:30:51 -0400

    correccion if

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -5,9 +5,10 @@ var mensaje = document.getElementById("mensaje");
 var boton = document.getElementById("enviar");
 
 //Se crea evento para el botón enviar
-
-if(
-    nombre = ""; 
-    alert("Debe incluir un nombre válido")
+boton.addEventListener("click",function(){
+    if(nombre = "" ){
+        alert("Debe incluir un nombre válido")
+    }
+    
+}
 )
-
