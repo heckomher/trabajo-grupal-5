ebe19354be0df1d832ec1acb1328e44f3813237a
Hector Komori <hector.komori@gmail.com>
2023-04-27 21:19:30 -0400

    carga de archivos desde html a JS

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -1,1 +1,13 @@
-var initial ="0";
+//se traen los datos desde el formulario
+var nombre = document.getElementById("nombre");
+var correo = document.getElementById("email");
+var mensaje = document.getElementById("mensaje");
+var boton = document.getElementById("enviar");
+
+//Se crea evento para el botón enviar
+
+if(
+    nombre = ""; 
+    alert("Debe incluir un nombre válido")
+)
+
